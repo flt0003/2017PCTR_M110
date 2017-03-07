aa0513c9716b436a5a5b7e68b2f62c57bcc9e0c9
jct0024 <jct0024@alu.ubu.es>
2017-03-07 19:13:21 +0100

    Prueba 1

--- 2017PCTR_M110_P/src/p012/BallThread.java
+++ 2017PCTR_M110_P/src/p012/BallThread.java
@@ -1,7 +1,21 @@
 package p012;
 
-public class BallThread extends Thread {
-	public BallThread(Ball b){
-		
+
+
+public class BallThread extends Thread{
+	private final Ball bola;
+	public BallThread(Ball bola){
+		this.bola=bola;
+	}
+	public void run(){
+		try {
+			while (!this.isInterrupted()){
+				bola.move();
+				Thread.sleep(50);
+			}
+		} catch (InterruptedException e) {
+			System.out.println(this.getName()+" interrumpido");
+		}
 	}
 }
+
--- 2017PCTR_M110_P/src/p012/PainterThread.java
+++ 2017PCTR_M110_P/src/p012/PainterThread.java
@@ -1,5 +1,20 @@
 package p012;
 
-public class PainterThread extends Thread {
 
+public class PainterThread extends Thread {
+	private final Board b;
+	public PainterThread(Board b){
+		this.b=b;
+	}
+	@Override
+	public void run(){
+		try {
+			while(!this.isInterrupted()){
+				b.repaint();
+				Thread.sleep(1);
+			}
+		} catch (InterruptedException e) {
+			System.out.println(this.getName()+" interrumpido");
+		}
+	}
 }
